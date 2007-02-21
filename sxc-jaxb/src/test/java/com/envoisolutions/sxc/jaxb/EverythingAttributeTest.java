7cefc18cc3d2f6429ed1bf7fb29a59c4356dab42
dandiep <dandiep@be261fe8-9e29-0410-9bf4-d7d028d7064b>
2007-02-21 23:21:21 +0000

    Add missing files.

---
+++ sxc-jaxb/src/test/java/com/envoisolutions/sxc/jaxb/EverythingAttributeTest.java
@@ -0,0 +1,63 @@
+package com.envoisolutions.sxc.jaxb;
+
+import java.io.ByteArrayOutputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+
+import org.w3c.dom.Document;
+
+import com.envoisolutions.sxc.util.Base64;
+import com.envoisolutions.sxc.util.XoTestCase;
+import com.everything.AttributesType;
+
+public class EverythingAttributeTest extends XoTestCase {
+    
+    @SuppressWarnings("unchecked")
+    public void testJAXBContextUnmarshal() throws Exception {
+        System.setProperty("streax-xo.output.directory", "target/tmp-jaxb");
+        JAXBContextImpl ctx = JAXBContextImpl.createContext("com.everything", getClass().getClassLoader(), null);
+        
+        
+        JAXBElement<AttributesType> a = (JAXBElement<AttributesType>) 
+            ctx.createUnmarshaller().unmarshal(getClass().getResourceAsStream("everything-attribute.xml"));
+        assertNotNull(a);
+        
+        AttributesType st = a.getValue();
+        assertNotNull(st);
+        assertEquals("urn:test", st.getAnyURI());
+        assertTrue(Arrays.equals(Base64.decode("0123456789ABCDEF"), st.getBase64Binary()));
+        assertTrue(st.isBoolean());
+        assertEquals(new BigDecimal("123.123"), st.getDecimal());
+        assertEquals(123.123, st.getDouble());
+        assertEquals(1.2e-2, st.getFloat(), 1.0e-4);
+        assertEquals(123, st.getInt().intValue());
+        assertEquals(new BigInteger("123"), st.getInteger());
+        assertEquals("en", st.getLanguage());
+        assertEquals(new QName("http://everything.com", "FooBar"), st.getQName());
+        assertEquals(new QName("http://everything.com", "FooBar"), st.getQNameNoPrefix());
+        
+        Marshaller marshaller = ctx.createMarshaller();
+        assertNotNull(marshaller);
+        
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        marshaller.marshal(a, bos);
+        
+        Document d = readDocument(bos.toByteArray());
+        addNamespace("e", "http://everything.com");
+        addNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        assertValid("/e:attributes", d);
+        assertValid("/e:attributes[@base64Binary='0123456789ABCDEF']", d);
+        assertValid("/e:attributes[@decimal='123.123']", d);
+        assertValid("/e:attributes[@duration='P1347Y']", d);
+        assertValid("/e:attributes[@double='123.123']", d);
+        assertValid("/e:attributes[@float='0.012']", d);
+        assertValid("/e:attributes[@int='123']", d);
+        assertValid("/e:attributes[@integer='123']", d);
+        assertValid("/e:attributes[@string='string']", d);
+    }
+}
---
+++ sxc-jaxb/src/test/java/com/envoisolutions/sxc/jaxb/typesub/EmployeeTest.java
@@ -0,0 +1,61 @@
+package com.envoisolutions.sxc.jaxb.typesub;
+
+import java.io.ByteArrayOutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.MarshalException;
+import javax.xml.bind.Marshaller;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.w3c.dom.Document;
+
+import com.ctc.wstx.stax.WstxInputFactory;
+import com.ctc.wstx.stax.WstxOutputFactory;
+import com.envoisolutions.sxc.jaxb.JAXBContextImpl;
+import com.envoisolutions.sxc.util.XoTestCase;
+
+import xfire.inheritance.Employee;
+
+public class EmployeeTest extends XoTestCase {
+    private WstxInputFactory xif = new WstxInputFactory();
+    private XMLOutputFactory xof = new WstxOutputFactory();
+    
+    public void testEmployee() throws Exception {
+        System.setProperty("streax-xo.output.directory", "target/tmp-jaxb");
+        
+        JAXBContext ctx = new JAXBContextImpl(new Class[] { Employee.class } );
+        
+        JAXBElement<Employee> jaxbE = (JAXBElement<Employee>)
+            ctx.createUnmarshaller().unmarshal(
+                          xif.createXMLStreamReader(getResourceAsStream("employee.xml")), Employee.class);
+        assertNotNull(jaxbE);
+        assertNotNull(jaxbE.getValue());
+        
+        Employee e = jaxbE.getValue();
+        assertEquals("foo", e.getDivision());
+        assertEquals("bar", e.getName());
+
+        Marshaller marshaller = ctx.createMarshaller();
+        assertNotNull(marshaller);
+        
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        XMLStreamWriter writer = xof.createXMLStreamWriter(bos);
+        try {
+            marshaller.marshal(e, writer);
+            fail("MarshalException should have been thrown!");
+        } catch (MarshalException ex) {
+        }
+        
+        marshaller.marshal(jaxbE, writer);
+        
+        writer.close();
+        System.out.println(bos.toString());
+        Document d = readDocument(bos.toByteArray());
+        addNamespace("i", "urn:xfire:inheritance");
+        assertValid("/i:in0/i:name[text()='bar']", d);
+        assertValid("/i:in0/i:division[text()='foo']", d);
+    }
+    
+}
