a498719433a42e671694f84c2c73a096899140a7
dandiep <dandiep@be261fe8-9e29-0410-9bf4-d7d028d7064b>
2007-03-06 20:00:29 +0000

    Snippets.

--- sxc-drools/src/test/java/com/envoisolutions/sxc/drools/DroolsTest.java
+++ sxc-drools/src/test/java/com/envoisolutions/sxc/drools/DroolsTest.java
@@ -9,23 +9,19 @@ import com.envoisolutions.sxc.xpath.XPathEvaluator;
 import com.envoisolutions.sxc.xpath.XPathEventHandler;
 
 public class DroolsTest extends TestCase {
-    String value = null;
 
     public void testDan() throws Exception {
         System.setProperty("streax-xo.output.directory", "target/tmp-xpath");
 
+        // START SNIPPET: rules
         final DroolsXPathEvaluatorFactory evaluatorFactory = 
             new DroolsXPathEvaluatorFactory();
         evaluatorFactory.setPackageStream(getClass().getResourceAsStream("test.drl"));
         evaluatorFactory.addPrefix("c", "urn:customer");
         
         XPathEvaluator evaluator = evaluatorFactory.create();
-        // FactHandle someFactHandle = evaluator.assertObject(someObject);
         evaluator.evaluate(getClass().getResourceAsStream("test.xml"));
-        // evaluator.getObject(someFactHandle);
-//        idHandler.getWorkingMemory().fireAllRules();
-
-
+        // END SNIPPET: rules
     }
 
 
