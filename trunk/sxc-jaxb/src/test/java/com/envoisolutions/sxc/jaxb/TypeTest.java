ee858f72444be473c4a7cde6941221f4e0ec7e3c
dandiep <dandiep@be261fe8-9e29-0410-9bf4-d7d028d7064b>
2007-03-12 05:23:38 +0000

    Add missing file.

---
+++ trunk/sxc-jaxb/src/test/java/com/envoisolutions/sxc/jaxb/TypeTest.java
@@ -0,0 +1,41 @@
+package com.envoisolutions.sxc.jaxb;
+
+import java.util.HashMap;
+
+import com.envoisolutions.sxc.util.XoTestCase;
+
+import org.apache.type_test.types1.DecimalEnum;
+
+public class TypeTest extends XoTestCase {
+    public void testEverything1() throws Exception {
+        System.setProperty("streax-xo.output.directory", "target/tmp-jaxb");
+        JAXBContextImpl ctx = JAXBContextImpl.createContext("org.apache.type_test.types1", getClass()
+            .getClassLoader(), null);
+
+        assertNotNull(ctx);
+    }
+
+    public void testEverything2() throws Exception {
+        System.setProperty("streax-xo.output.directory", "target/tmp-jaxb");
+        JAXBContextImpl ctx = JAXBContextImpl.createContext("org.apache.type_test.types2", getClass()
+            .getClassLoader(), null);
+
+        assertNotNull(ctx);
+    }
+
+    public void testEverything3() throws Exception {
+        System.setProperty("streax-xo.output.directory", "target/tmp-jaxb");
+        JAXBContextImpl ctx = JAXBContextImpl.createContext("org.apache.type_test.types3", getClass()
+            .getClassLoader(), null);
+
+        assertNotNull(ctx);
+    }
+
+    public void xtestSingleEnum() throws Exception {
+        System.setProperty("streax-xo.output.directory", "target/tmp-jaxb");
+        JAXBContextImpl ctx = JAXBContextImpl.createContext(new Class[] {DecimalEnum.class},
+                                                            new HashMap<String, Object>());
+
+        assertNotNull(ctx);
+    }
+}
