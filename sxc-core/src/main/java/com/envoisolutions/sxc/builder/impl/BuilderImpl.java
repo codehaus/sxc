e96e80a574b638b92e6bc3b6605500a4aae4e912
kohsuke <kohsuke@be261fe8-9e29-0410-9bf4-d7d028d7064b>
2007-07-04 02:01:38 +0000

    added Context class generation.

--- sxc-core/src/main/java/com/envoisolutions/sxc/builder/impl/BuilderImpl.java
+++ sxc-core/src/main/java/com/envoisolutions/sxc/builder/impl/BuilderImpl.java
@@ -1,6 +1,8 @@
 package com.envoisolutions.sxc.builder.impl;
 
 import com.envoisolutions.sxc.Context;
+import com.envoisolutions.sxc.Reader;
+import com.envoisolutions.sxc.Writer;
 import com.envoisolutions.sxc.builder.BuildException;
 import com.envoisolutions.sxc.builder.Builder;
 import com.envoisolutions.sxc.builder.ElementParserBuilder;
@@ -10,6 +12,10 @@ import com.envoisolutions.sxc.compiler.JavacCompiler;
 import com.envoisolutions.sxc.util.Util;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JClassAlreadyExistsException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -23,6 +29,7 @@ public class BuilderImpl implements Builder {
     private BuildContext buildContext;
     private ElementWriterBuilderImpl writerBuilder;
     private Compiler compiler = new JavacCompiler();
+    private String contextClassName;
 
     /**
      *
@@ -32,20 +39,40 @@ public class BuilderImpl implements Builder {
      * @param writerClassName
      *      Fully qualified class name of the writer to be generated.
      *      null to not generate the writer.
+     * @param contextClassName
+     *      Fully qualified class name of the {@link Context}-derived class to be generated.
+     *      null to not generate one.
      */
-    public BuilderImpl(String readerClassName, String writerClassName) {
+    public BuilderImpl(String readerClassName, String writerClassName, String contextClassName) {
         this.buildContext = new BuildContext();
         if(readerClassName!=null)
             parserBuilder = new ElementParserBuilderImpl(buildContext,readerClassName);
         if(writerClassName!=null)
             writerBuilder = new ElementWriterBuilderImpl(buildContext,writerClassName);
+
+        if(contextClassName!=null) {
+            try {
+                JDefinedClass cc = buildContext.getCodeModel()._class(contextClassName);
+                cc.method(JMod.PUBLIC, Reader.class,"createReader")
+                    .body()._return( parserBuilder!=null
+                        ? JExpr._new(parserBuilder.getReaderClass())
+                        : JExpr._null());
+                cc.method(JMod.PUBLIC, Writer.class,"createWriter")
+                    .body()._return( writerBuilder!=null
+                        ? JExpr._new(writerBuilder.getWriterClass())
+                        : JExpr._null());
+            } catch (JClassAlreadyExistsException e) {
+                throw new BuildException(e);
+            }
+        }
+        this.contextClassName = contextClassName;
     }
 
     /**
      * Generates both reader and writer by using default names. 
      */
     public BuilderImpl() {
-        this("generated.sxc.Reader","generated.sxc.Writer");
+        this("generated.sxc.Reader","generated.sxc.Writer","generated.sxc.Context");
     }
 
     public ElementParserBuilder getParserBuilder() {
@@ -114,7 +141,8 @@ public class BuilderImpl implements Builder {
         if (delete && file == null) {
             Util.delete(dir);
         }
-        
+
+        // TODO: simply load the generated context class and get rid of CompiledContext
         return new CompiledContext(cl,
             parserBuilder!=null ? parserBuilder.readerClass.fullName() : null,
             writerBuilder!=null ? writerBuilder.getWriterClass().fullName() : null);
