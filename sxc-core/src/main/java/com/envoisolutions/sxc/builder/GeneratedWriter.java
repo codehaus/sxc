8a1a1f79528f927d87db4c54aab915e4634fabb9
kohsuke <kohsuke@be261fe8-9e29-0410-9bf4-d7d028d7064b>
2007-07-04 01:42:15 +0000

    these classes are now unused.

--- sxc-core/src/main/java/com/envoisolutions/sxc/builder/GeneratedWriter.java
+++
@@ -1,8 +0,0 @@
-package com.envoisolutions.sxc.builder;
-
-import com.envoisolutions.sxc.Context;
-import com.envoisolutions.sxc.util.XoXMLStreamWriter;
-
-public interface GeneratedWriter {
-    public void write(XoXMLStreamWriter reader, Context context, Object o) throws Exception;
-}
