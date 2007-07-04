eff89ca5e83e8f815a528a3d187c947f5b390f64
kohsuke <kohsuke@be261fe8-9e29-0410-9bf4-d7d028d7064b>
2007-07-04 08:26:09 +0000

    added another write method to allow better output redirection.

--- sxc-core/src/main/java/com/envoisolutions/sxc/builder/Builder.java
+++ sxc-core/src/main/java/com/envoisolutions/sxc/builder/Builder.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import com.envoisolutions.sxc.Context;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JMethod;
+import com.sun.codemodel.CodeWriter;
 
 
 public interface Builder {
@@ -13,10 +14,21 @@ public interface Builder {
     ElementParserBuilder getParserBuilder();
     
     ElementWriterBuilder getWriterBuilder();
-    
-    Context compile();
-    
+
+    /**
+     * Generates and compiles the code, then loads it back into the current JVM. 
+     */
+    Context compile() throws BuildException;
+
+    /**
+     * Generates the code into the given directory.
+     */
     void write(File file) throws BuildException, IOException;
+
+    /**
+     * Generates the code through a custom {@link CodeWriter}.
+     */
+    void write(CodeWriter writer) throws IOException, BuildException;
 
     JCodeModel getCodeModel();
 
